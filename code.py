fa518412eef691e4fd086e2d536f6ce986e2c31a
ablades <audaris@gmail.com>
2020-04-12 10:37:19 -0700

    sample problem

--- code.py
+++ code.py
@@ -6,6 +6,16 @@
 
 class Solution:
 
+    def dups():
+        # Given a list of n numbers, determine if it contains any duplicate numbers
+        duplicates = set()
+
+        for item in range(numbers):
+            if duplicates.find(item):
+                return True
+            else:
+                duplicates.add(item)
+    
 
 
     
