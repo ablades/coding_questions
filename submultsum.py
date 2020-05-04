f895630b37989edcc844f88b458cd04ff7c33772
ablades <audaris@gmail.com>
2020-05-03 21:03:11 -0700

    update variable table

--- submultsum.py
+++ submultsum.py
@@ -10,13 +10,6 @@ def subtractProductAndSum(self, n: int) -> int:
             
         return mult - sum
 
-        Input: n = 234
-Output: 15 
-Explanation: 
-Product of digits = 2 * 3 * 4 = 24 
-Sum of digits = 2 + 3 + 4 = 9 
-Result = 24 - 9 = 15
-
 """
 Variable Table 
 n = 234
