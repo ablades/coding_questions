6e7ea738d43e13f493c2a53f8368b85a8bf5fd1c
ablades <audaris@gmail.com>
2020-05-09 11:18:29 -0700

    add find numbers

---
+++ findnumbers.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/find-numbers-with-even-number-of-digits/submissions/
+def findNumbers(self, nums: List[int]) -> int:
+    count = 0
+    for _, num in enumerate(nums): #O(N)
+        if len(str(num)) % 2 == 0: #len O(1) operation in python str o(s)
+            count += 1 # O(1)
+            
+    return count #O(1)
+   
+   #Overall runtime O(N + S)
